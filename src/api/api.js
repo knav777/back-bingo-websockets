38831d5c87486d28723906ce829b504e059977e1
knav777 <45278122+knav777@users.noreply.github.com>
2021-08-23 03:26:11 -0400

    latest for the moment

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,8 @@ class Api{
     async requestPlay( req, res ){
 
         let resp = {};
+        var bbdd = require( '../users.json' );
+        let sockets_ids = [];
         var userid = 0;
 
         const io = require( '../index' ).VARS.io;
@@ -16,32 +18,53 @@ class Api{
 
             console.log( count );
 
-            /* if( count > 0 ){ */
-                console.log( 'countdown' )
-                let countdown = 5;
-                socket.emit( 'game:time', countdown );
+            if( ! sockets_ids.includes( socket.id ) ){
+                sockets_ids.push( socket.id );
 
-                setTimeout( () => {
-                    let numbers_selected = [];
+                if( count > 0 && bbdd.users.length > 0 ){
+                    console.log( 'countdown' )
+                    let countdown = 7;
+                    io.sockets.emit( 'game:time', countdown );
+    
+                    setTimeout( () => {
+                        let numbers_selected = [];
+    
+                        setInterval( () => {
+    
+                            do{
+                                var is_num_exists = true;
+                                var number = Api.generateRandom( 1, 75 );
+                                
+                                is_num_exists = numbers_selected.includes( number );                        
+    
+                            }while( is_num_exists );
 
-                    setInterval( () => {
 
-                        do{
-                            var number = Api.generateRandom( 1, 75 );
-                            var is_num_exists = numbers_selected.includes( number );                        
+                            if( numbers_selected.length >= 74 ){
+                                io.sockets.emit( 'game:over', null );
+                            }
+                            else{
+                                numbers_selected.push( number );
+                                io.sockets.emit( 'bingo:callNumber', number );
+                            }
+                            
 
-                        }while( is_num_exists );
+                        }, 500 );
+    
+                    }, (countdown * 1000) + 1000 );
+                }
+    
+                socket.on( 'bingo:callBingo', ( data ) => {
+    
+                    const bundle = {...data, io}
+                    Api.checkCartonWinner( bundle );
+                })
+    
+                socket.on("disconnect", () => {
+                    console.log("Client disconnected");
+                });
+            }
 
-                        numbers_selected.push( number );
-                        socket.emit( 'bingo:callNumber', number );
-                    }, 3000 );
-
-                }, (countdown * 1000) + 1000 );
-            //}
-
-            socket.on("disconnect", () => {
-                console.log("Client disconnected");
-            });
         });
 
         if( req.method === 'POST' ){
@@ -49,8 +72,10 @@ class Api{
             const bundle = {
                 "req": req,
                 "res": res,
-                "userid": 0
+                "userid": sockets_ids.length
             };
+
+            bbdd.users.push( bundle );
             Api.actions( bundle );
         }
 
@@ -74,35 +99,7 @@ class Api{
         if( action === 'join' ){
 
             let board = [];
-
-            for( let i = 0; i < 5 ; i++ ){
-                board[i] = [];
-
-                for( let j = 0; j < 5; j++ ){
-
-                    do{
-                        var rule = false;
-                        var is_num_exists = true;
-
-                        var num = Api.generateRandom( 1, 75 );
-                        is_num_exists = Api.existsThisNumberHere( num, board );
-                        
-                        if( j === 0 ) { rule = ( num >= 1 && num <= 15 ); }
-                        else if( j === 1 ) { rule = ( num >=16 && num <= 30 ); }
-                        else if( j === 2 ) { rule = ( num >=31 && num <= 45 ); }
-                        else if( j === 3 ) { rule = ( num >=46 && num <= 60 ); }
-                        else if( j === 4 ) { rule = ( num >=61 && num <= 75 ); }
-
-                    } while( ! rule || is_num_exists );
-
-                    if( i === 2 && j === 2 ){
-                        board[i][j] = 0;
-                    }
-                    else{
-                        board[i][j] = num;
-                    }
-                }
-            }
+            board = Api.generateCartonUnique();
 
             const resp = {
                 "action": "joined",
@@ -125,6 +122,67 @@ class Api{
         }
 
         return false;
+    }
+
+    static checkCartonWinner( bundle ){
+
+        let userid = bundle.userid;
+        let board = bundle.board;
+        let io = bundle.io;
+        let numbers_selected = bundle.boardSelected;
+
+        let posi_selected = [];
+        let cont = 0;
+
+        numbers_selected.forEach( item => {
+
+            for( let i = 0; i < board.length; i++ ){
+                for( let j = 0; j < board[i].length; j++ ){
+                    if( item === board[i][j] ){
+                        posi_selected[cont] = [ i, j ];
+                    }
+                }
+            }
+            cont++;
+        });
+
+        io.sockets.emit( 'game:over', userid );
+    }
+
+    static generateCartonUnique(){
+        
+        let board = [];
+
+        for( let i = 0; i < 5 ; i++ ){
+            board[i] = [];
+
+            for( let j = 0; j < 5; j++ ){
+
+                do{
+                    var rule = false;
+                    var is_num_exists = true;
+
+                    var num = Api.generateRandom( 1, 75 );
+                    is_num_exists = Api.existsThisNumberHere( num, board );
+                    
+                    if( j === 0 ) { rule = ( num >= 1 && num <= 15 ); }
+                    else if( j === 1 ) { rule = ( num >=16 && num <= 30 ); }
+                    else if( j === 2 ) { rule = ( num >=31 && num <= 45 ); }
+                    else if( j === 3 ) { rule = ( num >=46 && num <= 60 ); }
+                    else if( j === 4 ) { rule = ( num >=61 && num <= 75 ); }
+
+                } while( ! rule || is_num_exists );
+
+                if( i === 2 && j === 2 ){
+                    board[i][j] = 0;
+                }
+                else{
+                    board[i][j] = num;
+                }
+            }
+        }
+
+        return board;
     }
 
 }
